a6dbdd8803e9f5bb135b00aada7cd8222d3cdb2d
bernard-wang <bernard.wang@ibm.com>
2016-11-06 15:53:49 -0600

    Fix scrolling

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import 'babel-polyfill';
 
 global.app = function () {
+
+	// #people cycle
 	let words = [
 		'empathetic?',
 		'genuine?',
@@ -24,39 +26,40 @@ global.app = function () {
 	];
 
 	let i = 0;
-	let interval = null;
+	let people_cycle = null;
 
 	let stopPeopleCycle = function() {
-		clearInterval(interval);
-		interval = null;
+		clearInterval(people_cycle);
+		people_cycle = null;
 	}
 
 	let startPeopleCycle = function() {
-		if (interval != null) {
+		if (people_cycle != null) {
 			return;
 		}
-		interval = setInterval(function() {
+		people_cycle = setInterval(function() {
 			i = (i + 1) % words.length;
 			// background 
 			$('#people').css("background-image", "url(" + images[i = (i + 1) % images.length] + ")");
 			// character traits
 			$('#swap-word').fadeOut(function() {
-				$(this).html(words[i]).fadeIn();
-			});
+				$(this).html(words[i]);
+			}).fadeIn();
 			// people description
 			$('#swap-name').fadeOut(function() {
-				$(this).html(people[i][0]).fadeIn();
-			});
+				$(this).html(people[i][0]);
+			}).fadeIn();
 			$('#swap-role').fadeOut(function() {
-				$(this).html(people[i][1]).fadeIn();
-			});
+				$(this).html(people[i][1]);
+			}).fadeIn();
 			$('#swap-loc').fadeOut(function() {
-				$(this).html(people[i][2]).fadeIn();
-			});
+				$(this).html(people[i][2]);
+			}).fadeIn();
 		}, 3000);
 	}
 
-	let old_id = 0;     // current page index
+
+	// site navigation
 	let pages = [       // array of pages
 		'#maelstrom',
 		'#overview',
@@ -65,17 +68,54 @@ global.app = function () {
 		'#people',
 		'#apply'
 	];
-	let timer = null;   // scroll timer
-	let count = 0;      // scroll counter
+
+	let page_id = 0;						// current page index
+
+	let scroll_time = new Date().getTime();	// last scroll timestamp
+	let scroll_cooldown = 1000; 			// 1 seconds in between mousewheel events
+	let arrow_cooldown = 500; 				// .5 seconds in between keydown events
+
+	let scroll_count = 0;     				// scroll event counter
+	let scroll_threshold = 30;				// minimum delta Y to register scroll event
+
+	/**
+	 * Switch to given page id
+	 */
+	let scrollPage = function(new_id) {
+		if (new_id == page_id) return false;
+
+		// people page switching
+		if (new_id == 4) {
+			startPeopleCycle();
+		} else if (page_id == 4) {
+			stopPeopleCycle();
+		}
+
+		let new_page = pages[new_id];
+		let old_page = pages[page_id];
+		let new_nav = $('#nav').children()[new_id];
+		let old_nav = $('#nav').children()[page_id];
+
+		$(new_nav).addClass('target');
+		$(old_nav).removeClass('target');
+
+		$(new_page).addClass('target');
+		setTimeout(function(){
+			$(old_page).removeClass('target');
+		}, 100);
+
+		page_id = new_id;
+		scroll_count = 0;
+		scroll_time = new Date().getTime();
+	}
 
 	/**
 	 * Navs to next page
 	 */
 	let nextPage = function() {
-		if (old_id < pages.length-1) {
-			let new_id = old_id+1;
-			switchPage(new_id);
-			if (timer !== null) clearTimeout(timer);
+		if (page_id < pages.length-1) {
+			let new_id = page_id+1;
+			scrollPage(new_id);
 		}
 	}
 
@@ -83,71 +123,36 @@ global.app = function () {
 	 * Navs to prev page
 	 */
 	let prevPage = function() {
-		if (old_id > 0) {
-			let new_id = old_id-1;
-			switchPage(new_id);
-			if (timer !== null) clearTimeout(timer);
+		if (page_id > 0) {
+			let new_id = page_id-1;
+			scrollPage(new_id);
 		}
 	}
 
 	/**
-	 * Switch to given page id
+	 * Checks if past cooldown
 	 */
-	let switchPage = function(new_id) {
-		if (new_id !== old_id) {
-			if (new_id == 4) {
-				startPeopleCycle();
-			} else if (old_id == 4) {
-				stopPeopleCycle();
-			}
-
-			let new_page = pages[new_id];
-			let old_page = pages[old_id];
-			let new_nav = $('#nav').children()[new_id];
-			let old_nav = $('#nav').children()[old_id];
-
-			$(new_nav).addClass('target');
-			$(old_nav).removeClass('target');
-
-			$(new_page).addClass('target');
-			setTimeout(function(){
-				$(old_page).removeClass('target');
-			}, 100);
-
-			old_id = new_id;
-		}
+	let pastCooldown = function(cooldown) {
+		let current_time = new Date().getTime();
+		if ((current_time - scroll_time) < cooldown) return false;
+		return true;
 	}
-
-	// nav setup
-	$('.nav-link').click(function(e) {   
-		e.preventDefault();
-		let new_id = $('.nav-link').index($(this));
-		switchPage(new_id);
-	});
 
 	// scroll setup
 	$('body').mousewheel(function(e) {
 		// ignore smaller movements
-		if (Math.abs(e.deltaY) < 30) return;
+		let scroll_value = e.deltaY;
+		if (Math.abs(scroll_value) < scroll_threshold) return;
 
-		// update direction count
-		if (e.deltaY < 0) {
-			count += 1;
-		} else if (e.deltaY > 0) {
-			count -= 1;
+		// ignore if not past cooldown
+		if (!pastCooldown(scroll_cooldown)) return;
+		
+		// scrolls
+		if (scroll_value < 0) {
+			nextPage();
+		} else {
+			prevPage();
 		}
-
-		// scroll timer
-		if (timer !== null) clearTimeout(timer);    // still scrolling
-		timer = setTimeout(function() {             // function runs when scrolling stops
-			let count_threshold = 5;
-			if (count > count_threshold) {         
-				nextPage();
-			} else if (count < -count_threshold) {
-				prevPage();
-			}
-			count = 0;
-		}, 30);
 	});
 
 	// arrow setup
@@ -155,8 +160,17 @@ global.app = function () {
 		nextPage();
 	});
 
+	// nav setup
+	$('.nav-link').click(function(e) {   
+		e.preventDefault();
+		let new_id = $('.nav-link').index($(this));
+		scrollPage(new_id);
+	});
+
 	// key binding for up and down arrows
 	$(document).keydown(function(e){
+		if (!pastCooldown(arrow_cooldown)) return;
+		
 		if (e.keyCode == 38) {
 			prevPage();
 		} else if (e.keyCode == 40) {
